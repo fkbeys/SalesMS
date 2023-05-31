090cf90e91c2e0ef115786b07fc1f65d74a2e812
fkbeys <fkbeys@gmail.com>
2023-05-31 11:01:37 +0200

    Basket Api created

---
+++ Services/Basket/SalesMS.Services.Basket.BasketApi/Controllers/ValuesController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SalesMS.Shared.SharedClass.BaseClasses;
+
+namespace SalesMS.Services.Basket.BasketApi.Controllers
+{ 
+    public class ValuesController : GenericBaseController
+    {
+
+
+
+    }
+}
