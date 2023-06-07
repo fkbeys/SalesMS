fb1c17258a4eb3804698a5f29cf10d6eb05265c9
fkbeys <fkbeys@gmail.com>
2023-06-07 23:23:47 +0200

    changes in docker-compose and docker files for frontend

--- FrontEnds/salesms_front_end/src/Components/Inputs/GenericMobilePhoneInput.tsx
+++ FrontEnds/salesms_front_end/src/Components/Inputs/GenericMobilePhoneInput.tsx
@@ -1,27 +1,36 @@
-import MuiPhoneNumber from 'material-ui-phone-number';
+// import MuiPhoneNumber from 'material-ui-phone-number';
 
-import { isValidPhoneNumber } from "libphonenumber-js";
-import React, { useState } from "react";
+// import { isValidPhoneNumber } from "libphonenumber-js";
+// import React, { useState } from "react";
 
 
-interface model {
-    nomre: string
-    setNomre: Function
-}
+// interface model {
+//     nomre: string
+//     setNomre: Function
+// }
 
-const GenericMobilePhoneInput = (model: model) => {
+// const GenericMobilePhoneInput = (model: model) => {
 
+//     return (
+
+//         <MuiPhoneNumber fullWidth size="small" value={model?.nomre} label="Mobil Nömre"
+//             placeholder="Mobil Nömre" variant="outlined" defaultCountry={'az'}
+//             onChange={(c: any) => {
+//                 // console.log(c, isValidPhoneNumber(c));
+//                 model?.setNomre(c);  //setNomre(c);
+
+//                 return true;
+//             }}
+//         />
+//     )
+// }
+
+// export default GenericMobilePhoneInput
+import React from 'react'
+
+const GenericMobilePhoneInput = () => {
     return (
-
-        <MuiPhoneNumber fullWidth size="small" value={model?.nomre} label="Mobil Nömre"
-            placeholder="Mobil Nömre" variant="outlined" defaultCountry={'az'}
-            onChange={(c: any) => {
-                // console.log(c, isValidPhoneNumber(c));
-                model?.setNomre(c);  //setNomre(c);
-
-                return true;
-            }}
-        />
+        <div>GenericMobilePhoneInput</div>
     )
 }
 
